8dcee177020dd2f2d0024d4711846af706561732
abidzan <abidzan@databay.de>
2024-03-28 22:16:40 +0100

    install shadcn + other dependencies + finish main screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,96 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="fis flex flex-col w-full min-h-screen relative">
+      <div className="fis-header flex flex-col p-7 relative text-white h-[45vh] w-full bg-gray-800">
+        <div className="fis-row flex justify-between h-[25%]">
+          <h2 className="text-6xl">
+            <span className="font-semibold">ICE 614</span> nach
+          </h2>
+          <h2 className="text-6xl">14:09</h2>
+        </div>
+        <div className="fis-row flex">
+          <h1 className="mt-7 font-bold" style={{ fontSize: "11rem" }}>
+            Hamburg Hbf
+          </h1>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="fis-content h-[55vh] shadow-md bg-slate-200 p-7">
+        <div className="w-full h-full bg-white rounded-xl overflow-hidden">
+          <div className="content-row flex w-full h-[75px]">
+            <div className="graphic mx-8 w-[12px] h-full relative">
+              <div className="line w-full h-full bg-gray-800"></div>
+            </div>
+            <div className="content flex justify-between items-center flex-1 pe-10 text-3xl font-semibold text-gray-500">
+              <div className="left">
+                <h3>Fahrtverlauf</h3>
+              </div>
+              <div className="right">
+                <h3>Abfahrt in 3 min</h3>
+              </div>
+            </div>
+          </div>
+          <div className="content-row flex w-full h-[75px]">
+            <div className="graphic mx-8 w-[12px] h-full relative flex items-center justify-center">
+              <div className="line w-full h-full border border-y-0 border-gray-800">
+                <div className="partialLine h-[50%] w-full bg-gray-800"></div>
+              </div>
+              <div className="ball w-[25px] h-[25px] rounded-full border-[4px] bg-white absolute border-gray-800"></div>
+            </div>
+            <div className="content flex justify-between items-center flex-1 pe-10 text-4xl font-semibold text-gray-500">
+              <div className="left">
+                <h3>Köln Hbf</h3>
+              </div>
+              <div className="right">
+                <h3 className="text-green-800 font-bold">14:12</h3>
+              </div>
+            </div>
+          </div>
+          <div className="content-row flex w-full h-[75px]">
+            <div className="graphic mx-8 w-[12px] h-full relative flex items-center justify-center">
+              <div className="line w-full h-full border border-y-0 border-gray-800"></div>
+              <div className="ball w-[25px] h-[25px] rounded-full border-[4px] bg-white absolute border-gray-800"></div>
+            </div>
+            <div className="content flex justify-between items-center flex-1 pe-10 text-4xl font-semibold text-gray-500">
+              <div className="left">
+                <h3>Solingen Hbf</h3>
+              </div>
+            </div>
+          </div>
+          <div className="content-row flex w-full h-[75px]">
+            <div className="graphic mx-8 w-[12px] h-full relative flex items-center justify-center">
+              <div className="line w-full h-full border border-y-0 border-gray-800"></div>
+              <div className="ball w-[25px] h-[25px] rounded-full border-[4px] bg-white absolute border-gray-800"></div>
+            </div>
+            <div className="content flex justify-between items-center flex-1 pe-10 text-4xl font-semibold text-gray-500">
+              <div className="left">
+                <h3>Wuppertal Hbf</h3>
+              </div>
+            </div>
+          </div>
+          <div className="content-row flex w-full h-[75px]">
+            <div className="graphic mx-8 w-[12px] h-full relative flex items-center justify-center">
+              <div className="line w-full h-full border border-y-0 border-gray-800"></div>
+              <div className="ball w-[25px] h-[25px] rounded-full border-[4px] bg-white absolute border-gray-800"></div>
+            </div>
+            <div className="content flex justify-between items-center flex-1 pe-10 text-4xl font-semibold text-gray-500">
+              <div className="left">
+                <h3>Hagen Hbf</h3>
+              </div>
+            </div>
+          </div>
+          <div className="content-row flex w-full h-[75px]">
+            <div className="graphic mx-8 w-[12px] h-full relative flex items-center justify-center">
+              <div className="line w-full h-full border border-y-0 border-gray-800"></div>
+              <div className="ball w-[25px] h-[25px] rounded-full border-[4px] bg-white absolute border-gray-800"></div>
+            </div>
+            <div className="content flex justify-between items-center flex-1 pe-10 text-4xl font-semibold text-gray-500">
+              <div className="left">
+                <h3>Dortmund Hbf</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
